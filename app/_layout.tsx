963b1e0558e5b20b31689d1a7de8ff20f731ca6f
nandovillar <nandovillar14@gmail.com>
2026-05-02 12:14:23 +0200

    feat: new functions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,14 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { supabase } from "@/src/lib/supabase";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -15,6 +16,20 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const router = useRouter();
+
+  // 🔥 Detectar cuando el usuario viene desde el email de recuperación
+  useEffect(() => {
+    const { data: subscription } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (event === "PASSWORD_RECOVERY") {
+          router.replace("/reset-password");
+        }
+      },
+    );
+
+    return () => subscription.subscription.unsubscribe();
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
